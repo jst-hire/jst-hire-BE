5ae1fadcc3ec7352f48337918a162672d646ce82
jst-hire <hirejst@gmail.com>
2023-06-09 10:01:31 +0530

    setup

---
+++ api/registration/registration.py
@@ -0,0 +1,44 @@
+
+import os
+import json
+import hashlib
+import sys
+import traceback
+from datetime import datetime
+from django.http import HttpResponse
+from JSTHIRE import settings
+from rest_framework import generics
+from api.registration import registration_queries as qry
+
+
+class Registration(generics.GenericAPIView):
+    """
+        API Name                   : Register user
+        Description                : This api is used to user registration. Return json object as response to
+                                    display data in UI.
+        Created By                 : Bharath
+        Created Date               : 03-06-2023
+        Last Modified By           :
+        Last Modified Date         :
+        Modification Description   :
+        """
+    permission_classes = ()
+    def __init__(self):
+        # logger.info("Registration API Service Started")
+        print("Registration API Service Started")
+
+    def post(self, request):
+        try:
+            first_name = request.data['first_nm']
+            last_name = request.data['last_nm']
+            rec_cre_ts = datetime.now()
+            result={'first_name':first_name,'last_name':last_name,'rec_created_time':rec_cre_ts}
+            return HttpResponse(json.dumps(result))
+        except ValueError:
+            return HttpResponse(result)
+        except Exception as err:
+            http_err = traceback.format_exc()
+            data = sys.exc_info()[1]
+            print(http_err)
+            # return HttpResponse(json.dumps({"error": http_err}))
+            return HttpResponse(err)
---
+++ api/urls.py
@@ -0,0 +1,10 @@
+
+from django.conf.urls import url
+from django.urls import path
+from . import views
+from .registration import registration
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('register', registration.Registration, name="register"),
+]
