d79896c8a062d5464ea56460d88cb06ab7b2a562
jst-hire <hirejst@gmail.com>
2023-06-09 13:01:00 +0530

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # jst-hire-BE
+
+1. setp clone the repo
+2. Open the project in pychamp and open terminal.
+3. create virtualenv 
+    commands
+    virtualenv env 
+        or
+    pip install virtualenv env
+4. next we need to activate
+    run this command
+    env\Scripts\activate
+5. pip install django
+6. next we need to add interpeter in setting in pychamp
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 
-from django.conf.urls import url
+#from django.conf.urls import url
 from django.urls import path
 from . import views
 from .registration import registration
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('register', registration.Registration, name="register"),
+    path('register', registration.Registration.as_view(), name="register"),
 ]
